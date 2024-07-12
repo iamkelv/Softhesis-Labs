d7c4cf32b8db9e15858184fa9302d0c370bda75d
amkelv <kelvin.m@notjustevent.com>
2024-07-12 15:16:28 +0100

    fixed: loader build error

--- src/components/atoms/loader/Loader.tsx
+++ src/components/atoms/loader/Loader.tsx
@@ -1,8 +1,6 @@
 import style from "./Loader.module.scss";
 import { useEffect } from "react";
 import "./Loader.css";
-import { Oval } from "react-loader-spinner";
-import { colors } from "../../../styles/colors";
 const Loader = ({ loading }: { loading: boolean }) => {
   useEffect(() => {
     document.body.style.overflow = loading ? "hidden" : "auto";
@@ -14,16 +12,7 @@ const Loader = ({ loading }: { loading: boolean }) => {
 
   return (
     <div className={style.container} id="loader-container">
-      <Oval
-        visible={true}
-        height="60"
-        width="60"
-        color={colors.pink100}
-        secondaryColor={colors.pink_T3}
-        ariaLabel="oval-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-      />
+      <span>Loading..</span>
     </div>
   );
 };
@@ -33,16 +22,7 @@ export default Loader;
 export const SpinalLoader = () => {
   return (
     <div className="loader_container">
-      <Oval
-        visible={true}
-        height="40"
-        width="40"
-        color={colors.pink_T3}
-        secondaryColor={colors.pink100}
-        ariaLabel="oval-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-      />
+      <span>Loading</span>      
     </div>
   );
 };
